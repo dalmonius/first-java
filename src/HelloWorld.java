a4f606ea0113fdea30340068eeed7cdf1f5d8912
Fábio Zunta <nonameeesg@gmail.com>
2022-08-09 19:44:51 -0300

    This are very weird still

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,10 @@
+/*
+ * This is my first Java program
+ */
+
 public class HelloWorld {
     public static void main (String[] args){
         System.out.println("Hello World!");
+        System.out.println("This is my first Java program :)");
     }
 }
